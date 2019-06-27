502f834ff7c7da163647a967583ccc4a942382ae
SantiagoAlbertoGiraldoEscobar <50932772+SantiagoAlbertoGiraldoEscobar@users.noreply.github.com>
2019-06-27 18:38:32 -0500

    Update palabras_unicas.py

--- palabras_unicas.py
+++ palabras_unicas.py
@@ -4,3 +4,6 @@ def palabras_unicas(l):
     palabraU = []
     [palabraU.append(x) for x in l if x not in palabraU]
     return palabraU
+
+# Para cambiar las palabras repetidas en una columna de una cadena
+# ' '.join(palabras_unicas(contenido[i+1].get_text(" ",strip=True).split()))
